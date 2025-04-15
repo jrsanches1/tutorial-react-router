33e11f9f4edc4902aa287fe18cdec15b2a5ea1ce
jrsanches1 <sanchesjoseruy@gmail.com>
2025-04-15 20:37:13 -0300

    Search params

--- src/route/Invoices/index.tsx
+++ src/route/Invoices/index.tsx
@@ -1,11 +1,13 @@
 
 import './styles.css';
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../data";
 
 export default function Invoices() {
 
     const Invoices = getInvoices();
+
+    const [searchParams, setSearchParams] = useSearchParams();
 
     return (
         <div style={{ display: "flex" }}>
@@ -15,15 +17,37 @@ export default function Invoices() {
                     padding: "1rem",
                 }}
             >
-                {Invoices.map((invoice) => (
-                    <NavLink
-                        className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
-                        to={`/invoices/${invoice.number}`}
-                        key={invoice.number}
-                    >
-                        {invoice.name}
-                    </NavLink>
-                ))}
+
+                <input
+                    value={searchParams.get("name") || ""}
+                    onChange={(event) => {
+                        const name = event.target.value;
+                        if (name) {
+                            setSearchParams({ name });
+                        } else {
+                            setSearchParams({});
+                        }
+                    }}
+                />
+
+                {Invoices
+                    .filter((invoice) => {
+                        const name = searchParams.get("name");
+                        if (!name) {
+                            return true;
+                        }
+                        const invoiceName = invoice.name.toLowerCase();
+                        return invoiceName.startsWith(name.toLowerCase());
+                    })
+                    .map((invoice) => (
+                        <NavLink
+                            className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+                            to={`/invoices/${invoice.number}`}
+                            key={invoice.number}
+                        >
+                            {invoice.name}
+                        </NavLink>
+                    ))}
             </nav>
             <Outlet />
         </div>
